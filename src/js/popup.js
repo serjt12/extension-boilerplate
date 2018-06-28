c9a47c935ae6273162edcf82f6768e9f74ad6cd2
serjt12 <jandreus87@hotmail.com>
2018-06-27 21:51:34 -0500

    Extension-boilerplate

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,8 @@
-import "../css/popup.css";
-import Greeting from "./popup/greeting_component.jsx";
-import React from "react";
-import { render } from "react-dom";
+import React from 'react'
+import Greeting from './popup/greeting_component.jsx'
+import { render } from 'react-dom'
 
 render(
-  <Greeting/>,
-  window.document.getElementById("app-container")
-);
+  <Greeting />,
+  window.document.getElementById('app-container')
+)
